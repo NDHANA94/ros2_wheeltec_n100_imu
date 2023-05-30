c8a64ee6226a387be86aaacbd4658e871d58f215
NDHANA94 <nipun.dhananjaya@gmail.com>
2023-05-31 00:58:44 +0300

    1st commit

---
+++ src/imu_node.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include "wheeltecN100_imu/imu_node.h"
+
+using namespace LibSerial;
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+  
+  printf("hello world wheeltecN100_imu package\n");
+  return 0;
+}
